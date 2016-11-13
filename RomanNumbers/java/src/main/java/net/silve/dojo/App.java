aa741eb2f9fbeb70377ba04dbea2a3bd044ac7d9
Robert Silve <rsilve@mailinblack.com>
2016-11-13 20:19:53 +0100

    int to roman conversion

--- RomanNumbers/java/src/main/java/net/silve/dojo/App.java
+++ RomanNumbers/java/src/main/java/net/silve/dojo/App.java
@@ -1,13 +1,29 @@
 package net.silve.dojo;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+public class App {
+
+    public static void main(String[] args) {
+        for (int i = 1; i < 101; i++) {
+            System.out.printf("%3d %10s\n", i, toRoman(i));
+        }
     }
+
+
+    public static String toRoman(Integer value) {
+        return new String(new char[value]).replace('\0', 'I')
+                .replaceAll("IIIII", "V")
+                .replaceAll("IIII", "IV")
+                .replaceAll("VV", "X")
+                .replaceAll("VIV", "IX")
+                .replaceAll("XXXXX", "L")
+                .replaceAll("XXXX", "XL")
+                .replaceAll("LL", "C")
+                .replaceAll("LXL", "XC")
+                .replaceAll("CCCCC", "D")
+                .replaceAll("CCCC", "CD")
+                .replaceAll("DD", "M")
+                .replaceAll("DCD", "CM");
+    }
+
 }
